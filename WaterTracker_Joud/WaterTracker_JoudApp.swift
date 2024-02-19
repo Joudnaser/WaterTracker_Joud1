397be8d68600dda1dd579c289071e4bafdf99c25
Joudnaser <125468777+Joudnaser@users.noreply.github.com>
2024-02-19 10:11:13 +0300

    Initial Commit

---
+++ WaterTracker_Joud/WaterTracker_JoudApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTracker_JoudApp.swift
+//  WaterTracker_Joud
+//
+//  Created by joud alkahtany on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WaterTracker_JoudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
